3dc17396ab6f16868c2445760216faa3aa6c93c2
Gyeonghun Park <gp112795@gmail.com>
2020-12-10 16:34:21 -0500

    ImageSearch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -20,6 +21,8 @@ function App() {
 
   return (
     <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
       {!isLoading && images.length === 0 && (
         <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
       )}
